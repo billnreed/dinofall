aa9aac598a2a73b45ccdea5930ff0120f0cdd47b
Bill Reed <billnreed@gmail.com>
2017-08-01 23:02:52 -0400

    dont stop moving after it hits a wall

--- src/entities/dinosaur.ts
+++ src/entities/dinosaur.ts
@@ -7,6 +7,8 @@ export class Dinosaur extends Phaser.Sprite {
     FACE_RIGHT: 1,
   };
 
+  private direction: number;
+
   constructor(game: Phaser.Game) {
     super(game, 0, 0, 'dinosaur');
 
@@ -20,15 +22,23 @@ export class Dinosaur extends Phaser.Sprite {
     if (!this.body.blocked.down) {
       this.body.velocity.y = 500;
     }
+
+    if (this.direction === Phaser.LEFT && !this.body.blocked.left) {
+      this.body.velocity.x = -500;
+    } else if (this.direction === Phaser.RIGHT && !this.body.blocked.right) {
+      this.body.velocity.x = 500;
+    }
   }
 
   public goLeft():void {
     this.frame = this.FRAMES.FACE_LEFT;
+    this.direction = Phaser.LEFT;
     this.body.velocity.x = -500;
   }
 
   public goRight():void {
     this.frame = this.FRAMES.FACE_RIGHT;
+    this.direction = Phaser.RIGHT;
     this.body.velocity.x = 500;
   }
 }
